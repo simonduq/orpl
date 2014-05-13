6ab62014c878e19fc6a07914d07faff0a9e4df6d
Simon Duquennoy <simonduq@sics.se>
2014-05-13 16:43:30 +0200

    Moved comment above the code it relates to

--- orpl2/orpl-routing-set.c
+++ orpl2/orpl-routing-set.c
@@ -76,12 +76,12 @@ get_hash(const uip_ipaddr_t *ipv6)
 {
   int i;
   uint64_t hash;
-  for(i=0; i<8; i++) {
-    ((unsigned char*)&hash)[i] = ipv6->u8[14+i%2];
-  }
   /* Initialize our hash using (the last bytes) of the IID
   rather than 0 for increased entropy (when initialized with 0 and hashing
   entries of only 16 bytes, SAX produces comparatively many collisions) */
+  for(i=0; i<8; i++) {
+    ((unsigned char*)&hash)[i] = ipv6->u8[14+i%2];
+  }
   for(i=0; i<16; i++) {
     /* Use only the IID, not the prefix, and loop twice over it as
      * only 8 iterations result in too little entropy with SAX */
