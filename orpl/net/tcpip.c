a86622cc6123b46c228d33222a2d0863e72534c1
Simon Duquennoy <simonduq@sics.se>
2014-07-02 12:58:20 +0200

    Automatically set an ORPL seqno when not done by application layer, making the use of orpl_set_curr_seqno from the application optional

--- orpl/net/tcpip.c
+++ orpl/net/tcpip.c
@@ -637,10 +637,15 @@ tcpip_ipv6_output(void)
 #else /* !WITH_ORPL */
       /* Set anycast MAC address instead of routing */
 
-      /* Get ORPL seqno as set by application */
       uint32_t seqno = orpl_get_curr_seqno();
-      if(seqno == 0) { /* We are not originator of the data, set
-      seqno of packet being forwarded */
+      if(uip_ds6_is_my_addr(&UIP_IP_BUF->srcipaddr)) {
+        /* We are originator of the data, check if there is
+         * a seqno already set by application layer */
+        if(seqno == 0) {
+          /* No seqno set, assign a new one */
+          seqno = orpl_get_new_seqno();
+        }
+      } else {
         seqno = orpl_packetbuf_seqno();
       }
       orpl_set_curr_seqno(seqno);
