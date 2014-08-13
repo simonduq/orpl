293382f555b8d94e7d4044ce78e7e2719f5d1f6e
Simon Duquennoy <simonduq@sics.se>
2014-08-13 13:47:46 +0200

    Make inc_hopcount and fp_hopcount safe

--- examples-full/tools/orpl-log.h
+++ examples-full/tools/orpl-log.h
@@ -76,8 +76,8 @@ void orpl_log_start();
 #define ORPL_LOG_FROM_PACKETBUF(...) ORPL_LOG_FROM_APPDATAPTR(appdataptr_from_packetbuf(), __VA_ARGS__)
 #define ORPL_LOG_IPADDR(addr) uip_debug_ipaddr_print(addr)
 #define ORPL_LOG_LLADDR(addr) uip_debug_lladdr_print(addr)
-#define ORPL_LOG_INC_HOPCOUNT_FROM_PACKETBUF() { appdataptr_from_packetbuf()->hop++; }
-#define ORPL_LOG_INC_FPCOUNT_FROM_PACKETBUF() { appdataptr_from_packetbuf()->fpcount++; }
+#define ORPL_LOG_INC_HOPCOUNT_FROM_PACKETBUF() { struct app_data *ptr = appdataptr_from_packetbuf(); if(ptr) ptr->hop++; }
+#define ORPL_LOG_INC_FPCOUNT_FROM_PACKETBUF() { struct app_data *ptr = appdataptr_from_packetbuf(); if(ptr) ptr->fpcount++; }
 
 #define ORPL_LOG_NODEID_FROM_RIMEADDR log_node_id_from_rimeaddr
 #define ORPL_LOG_NODEID_FROM_IPADDR log_node_id_from_ipaddr
